9c81ab43a86326743faf8dd3b801e2d8c86f09cc
rafzee <muhaiminbinmunir@gmail.com>
2018-09-04 15:39:25 +0600

    Second COmmit

--- app/src/main/java/com/example/muhamin/speechbee/MainActivity.java
+++ app/src/main/java/com/example/muhamin/speechbee/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.muhamin.speechbee;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        },SPLASH_TIME);
     }
+}
+
+void dummy(){
+    int x = 9;
 }
